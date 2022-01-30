462cdf5dfd851e6951c5fe9b0214b362ef432d8c
lefkosp <lefkoss@hotmail.com>
2022-01-30 16:09:32 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f9f90107-044e-47eb-955d-0ee8277a1727';
+export const MODAL_CLOSE_SEC = 2.5;
